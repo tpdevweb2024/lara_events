1bb07870a86b9fadecb6173d900ae54297cc601a
anthocodeur <antho.codeur@gmail.com>
2024-07-31 15:22:47 +0200

    add js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import "./bootstrap";
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menu.classList.contains("active")
+        ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "visible");
+});
